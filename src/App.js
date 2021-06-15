46721abaf341fec7acc8da8f3bdd7d6486c7d504
devamitjha <22409543+devamitjha@users.noreply.github.com>
2021-06-15 09:50:33 +0530

    swiper-slider
    swiper-slider

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/effect-coverflow/effect-coverflow.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "./styles.css";
+import SwiperCore, {
+  EffectCoverflow,
+  Pagination,
+  Navigation
+} from "swiper/core";
+
+SwiperCore.use([EffectCoverflow, Pagination, Navigation]);
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="title_wrapper">
+        <div className="reactLogo">
+          <img src="images/react.png" />
+        </div>
+        <div className="title_">
+          <span>React</span>Swiper Slider
+        </div>
+      </div>
+      <Swiper
+        navigation={true}
+        effect={"coverflow"}
+        centeredSlides={true}
+        slidesPerView={window.innerWidth < 768 ? 1 : "auto"}
+        loop={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true
+        }}
+        pagination={{
+          clickable: true
+        }}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="images/1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/6.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
